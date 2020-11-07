4f7fb87147ddee84e39d9210b2cb06a533dc9630
Windlin Armin <Armin.Windlin@swisscom.com>
2020-11-07 16:23:14 +0100

    basic movement, basic objects

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas ref="main-canvas">
+  <canvas ref="main-canvas" id="main-canvas" width="500" height="500">
     map
   </canvas>
 </template>
@@ -9,7 +9,20 @@ export default {
   name: 'Map',
   data() {
     return {
-      person: [],
+      you: {
+        x: 20,
+        y: 20
+      },
+      persons: [
+        {
+          x: 60,
+          y: 60
+        },
+        {
+          x: 60,
+          y: 120
+        }
+      ]
     };
   },
   methods: {
@@ -18,23 +31,51 @@ export default {
     },
     draw() {
       const ctx = this.$refs['main-canvas'].getContext('2d');
-      ctx.clearRect(0, 0, 300, 300); // clear canvas
+      ctx.clearRect(0, 0, 500, 500); // clear canvas
+
+      ctx.fillStyle = 'green';
+      ctx.fillRect(0, 0, 500, 500);
 
       ctx.fillStyle = 'red';
       ctx.beginPath();
-      ctx.arc(100, 100, 10, 0, 2 * Math.PI);
+      ctx.arc(this.you.x, this.you.y, 10, 0, 2 * Math.PI);
+      ctx.fill();
+
+      ctx.fillStyle = 'blue';
+      ctx.beginPath();
+      for (const person of this.persons) {
+        ctx.arc(person.x, person.y, 10, 0, 2 * Math.PI);
+      }
       ctx.fill();
 
       ctx.restore();
 
       window.requestAnimationFrame(this.draw);
+    },
+    onKeyPress(e) {
+      const key = e.key;
+      console.log(key)
+      if (key === 'w') {
+        this.you.y -= 10;
+      } else if (key === 's') {
+        this.you.y += 10;
+      } else if (key === 'a') {
+        this.you.x -= 10;
+      } else if (key === 'd') {
+        this.you.x += 10;
+      }
     }
   },
   mounted() {
     this.init();
+    window.addEventListener('keypress', this.onKeyPress)
   }
 }
 </script>
 
 <style scoped>
+#main-canvas {
+  width: 500px;
+  height: 500px;
+}
 </style>
