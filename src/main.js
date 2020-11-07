a7634fab021f569cabb1b14ff518aafd7fa7addb
Windlin Armin <Armin.Windlin@swisscom.com>
2020-11-07 19:34:52 +0100

    movement networking mvp

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import firebase from 'firebase'
+import firebase from './firebase'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$firebase = firebase
+Vue.prototype.$db = firebase.firestore();
+Vue.prototype.$users = firebase.firestore().collection('users');
 
 new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
-
-Vue.prototype.$firebase = firebase
