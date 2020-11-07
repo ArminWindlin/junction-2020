76546a1a4636922b0c056de2f345dfd6db9027a7
Windlin Armin <Armin.Windlin@swisscom.com>
2020-11-07 13:12:29 +0100

    basic canvas with circle

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,12 +1,38 @@
 <template>
-  <div>
+  <canvas ref="main-canvas">
     map
-  </div>
+  </canvas>
 </template>
 
 <script>
 export default {
-  name: 'Map'
+  name: 'Map',
+  data() {
+    return {
+      person: [],
+    };
+  },
+  methods: {
+    init() {
+      window.requestAnimationFrame(this.draw);
+    },
+    draw() {
+      const ctx = this.$refs['main-canvas'].getContext('2d');
+      ctx.clearRect(0, 0, 300, 300); // clear canvas
+
+      ctx.fillStyle = 'red';
+      ctx.beginPath();
+      ctx.arc(100, 100, 10, 0, 2 * Math.PI);
+      ctx.fill();
+
+      ctx.restore();
+
+      window.requestAnimationFrame(this.draw);
+    }
+  },
+  mounted() {
+    this.init();
+  }
 }
 </script>
 
